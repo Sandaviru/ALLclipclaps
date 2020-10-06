22019b1d9167914df12f5695e09e64bb83c994a4
Sandaviru <65564942+Sandaviru@users.noreply.github.com>
2020-10-06 18:18:52 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "ZjczYTJlMzEtNDA3Yi00MDkzLWI1ZTktMzQ5YzQxMmQzMzMy";
+$uuid = "54db1c16-1e76-48d0-a36d-eeb3a9d97056";
+$userid = "11033526";
+
+$eksternal_version = "1.9.9.1";
+$version = "42";
+$device = "6.0";
+$timezone = "5";
+$lang = "en";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; Android 6.0; CRO-U00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.101 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
